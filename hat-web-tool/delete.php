523ac4abea6d9e74742d42e70d84546c84409c98
maurelio <strigiwm@gmail.com>
2016-03-08 18:17:55 +0100

    Bring back hat-web-tools
    Change-Id: Ic8eb05bbce842b0086bf6736dee3ed5e5dc8f3e0

---
+++ hat-web-tool/delete.php
@@ -0,0 +1,130 @@
+<html>
+<head><title>Delete</title></head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "blue/style.css";</style>
+<script type="text/javascript" src="jquery-latest.js"></script>
+<script type="text/javascript" src="jquery.tablesorter.js"></script>
+<script type="text/javascript">
+$(document).ready(function()
+    {
+        $("#projects").tablesorter();
+    }
+);
+</script>
+<?php
+
+function titleLink ($title)
+{
+    return str_replace('%2F', '/', urlencode(str_replace(' ', '_', $title)));
+}
+function get_post($var) { return mysql_real_escape_string($_POST[$var]); }
+
+	require_once 'login.php';
+
+	$db_server = mysql_connect("metawiki.labsdb", $db_username, $db_password);
+	if (!$db_server) die ("Unable to connect to MySQL: " . mysql_error());
+
+	mysql_select_db("meta_p", $db_server) or die ("Unable to select database: " . mysql_error());
+	$admins = 0;
+
+	if (isset($_POST['number'])) {
+		$admins = get_post('number');
+	}
+	if ($admins > 10) {
+			$admins=10;
+	}
+?>
+<body>
+This will be a replacement for erwin85's delete tool.<br />
+<h2>WARNING: this is a beta tool. You are responsible for your own deletions; please check before you delete!</h2>
+<p>You can sort by multiple columns by first sorting by one column, and then shift-clicking the second column.</p>
+<hr />
+<form action="delete.php" method="post">
+Number of admins (maximum 10): <input type="text" name="number" value="<?php $admins ?>"/><br />
+<input type="submit" />
+</form>
+<table id="projects" class="tablesorter">
+<thead>
+<tr><th>Wiki</th><th>Admins</th><th>Last admin action</th><th>Page name</th><th>Last author</th><th>Last revision</th><th>Edit summary</th></tr>
+</thead>
+<tbody>
+	<?php
+
+
+	$query = "SELECT dbname,REPLACE(url, 'http://', 'https://') AS domain, slice FROM wiki WHERE url IS NOT NULL AND is_closed=0;";
+	$result = mysql_query($query);
+
+	if (!$result) die ("Database access failed: " . mysql_error());
+
+	$rows = mysql_num_rows($result);
+
+	for ($j = 0; $j < $rows; ++$j)
+	{
+		$row = mysql_fetch_row($result);
+
+		$db_server_temp = mysql_connect($row[2], $db_username, $db_password);
+		if (!$db_server_temp) die ("Unable to connect to MySQL: " . mysql_error());
+
+		mysql_select_db($row[0]."_p", $db_server_temp) or die ("Unable to select database: " . mysql_error());
+
+		$query2 = "SELECT sum(if(ug_group = 'sysop', 1, 0)) FROM user_groups;";
+		$result2 = mysql_query($query2);
+
+		if (!$result2) die ("Database access failed: " . mysql_error());
+
+		$row2 = mysql_fetch_row($result2);
+
+		$numAdmins = ($row2[0]?$row2[0]:0);
+
+		if ($numAdmins <= $admins) {
+
+			$queryL = "SELECT user_name, log_timestamp FROM logging JOIN user ON user_id = log_user JOIN user_groups ON ug_user = user_id WHERE log_type IN ('delete', 'block', 'protect') AND ug_group = 'sysop' ORDER BY log_timestamp DESC LIMIT 1;";
+			$resultL = mysql_query($queryL);
+
+			if (!$resultL) die ("Database access failed: " . mysql_error());
+
+			$rowL = mysql_fetch_row($resultL);
+
+			$query3 = "SELECT pl_title FROM pagelinks LEFT JOIN page ON page_id = pl_from WHERE page_title = 'Delete' AND page_namespace = 10 AND page_is_redirect = 1 LIMIT 1;";
+			$result3 = mysql_query($query3);
+
+			if (!$result3) die ("Database access failed: " . mysql_error());
+
+			$template = "Delete";
+
+			if (!$result3) {
+       		     $template = "Delete";
+       		} elseif (mysql_num_rows($result3) == 1) {
+        	    $template = mysql_result($result3, 0);
+       		} else {
+          	  $template = "Delete";
+        	}
+
+        	$query4 = "SELECT page_title, rev_timestamp, rev_user_text, rev_comment, rev_id FROM page LEFT JOIN templatelinks ON tl_from = page_id LEFT JOIN revision ON rev_page = page_id WHERE tl_title = '" . $template . "' AND tl_namespace=10 AND rev_timestamp = (SELECT max(rev_timestamp) FROM revision AS r WHERE rev_page = page_id)";
+			$result4 = mysql_query($query4);
+
+			if (!$result4) die ("Database access failed: " . mysql_error());
+
+			$rows4 = mysql_num_rows($result4);
+
+			for ($k = 0; $k < $rows4; ++$k)
+			{
+				$rowD = mysql_fetch_row($result4);
+				echo "<tr><td><a href=\"" . $row[1] . "\">". $row[0] . "</a></td>";
+				echo "<td>".$numAdmins."</td>\n";
+				echo "<td>".$rowL[1]."</td>\n";
+				echo "<td><a href=\"" . $row[1]. "/wiki/Special:Diff/".titleLink($rowD[4])."\">".$rowD[0]."</a></td>\n";
+				echo "<td><a href=\"" . $row[1]. "/wiki/User:".titleLink($rowD[2])."\">".$rowD[2]."</a></td>\n";
+				echo "<td>".$rowD[1]."</td>\n";
+				echo "<td>".$rowD[3]."</td></tr>\n";
+			}
+		}
+	}
+	?>
+
+</tbody>
+</table>
+<br />
+<p>This tool was originally written by <a href="https://meta.wikimedia.org/wiki/User:Rschen7754">Rschen7754</a>. Acknowledgements to <a href="http://tools.wmflabs.org/erwin85/">erwin85</a> for the original tool and many of the queries, and to <a href="http://tools.wmflabs.org/pathoschild-contrib/">Pathoschild</a> for creating the extensive suite of tools that were used as an example. Mantained by the stewardbots project.</p>
+</body>
+</html>
---
+++ hat-web-tool/projects.php
@@ -0,0 +1,67 @@
+<html>
+<head><title>Projects</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "blue/style.css";</style>
+<script type="text/javascript" src="jquery-latest.js"></script>
+<script type="text/javascript" src="jquery.tablesorter.js"></script>
+<script type="text/javascript">
+$(document).ready(function()
+    {
+        $("#projects").tablesorter();
+    }
+);
+</script>
+</head>
+<body>
+This is a replacement for erwin85's projects tool. For special rights, see <a href="http://tools.wmflabs.org/rightstool/cgi-bin/rightsstats">Dungodung's tool</a>.
+
+<br /><br />
+<table id="projects" class="tablesorter">
+<thead>
+<tr><th>Wiki</th><th>sysop</th><th>bureaucrat</th><th>checkuser</th><th>oversight</th></tr>
+</thead>
+<tbody>
+	<?php
+	require_once 'login.php';
+	$db_server = mysql_connect("metawiki.labsdb", $db_username, $db_password);
+	if (!$db_server) die ("Unable to connect to MySQL: " . mysql_error());
+
+	mysql_select_db("meta_p", $db_server) or die ("Unable to select database: " . mysql_error());
+
+	$query = "SELECT dbname,REPLACE(url, 'http://', 'https://') AS domain, slice FROM wiki WHERE url IS NOT NULL AND is_closed=0;";
+	$result = mysql_query($query);
+
+	if (!$result) die ("Database access failed: " . mysql_error());
+
+	$rows = mysql_num_rows($result);
+
+	for ($j = 0; $j < $rows; ++$j)
+	{
+		$row = mysql_fetch_row($result);
+
+		$db_server_temp = mysql_connect($row[2], $db_username, $db_password);
+		if (!$db_server_temp) die ("Unable to connect to MySQL: " . mysql_error());
+
+		mysql_select_db($row[0]."_p", $db_server_temp) or die ("Unable to select database: " . mysql_error());
+
+		$query2 = "SELECT sum(if(ug_group = 'sysop', 1, 0)), sum(if(ug_group = 'bureaucrat', 1, 0)), sum(if(ug_group = 'checkuser', 1, 0)), sum(if(ug_group = 'oversight', 1, 0)) FROM user_groups;";
+		$result2 = mysql_query($query2);
+
+		if (!$result2) die ("Database access failed: " . mysql_error());
+
+		$row2 = mysql_fetch_row($result2);
+
+		echo "<tr><td><a href=\"" . $row[1] . "\">". $row[0] . "</a></td>";
+		echo "<td><a href=\"" . $row[1]. "/wiki/Special:ListUsers/sysop\">". ($row2[0]?$row2[0]:0)."</td>\n";
+		echo "<td><a href=\"" . $row[1]. "/wiki/Special:ListUsers/bureaucrat\">".($row2[1]?$row2[1]:0)."</td>\n";
+		echo "<td><a href=\"" . $row[1]. "/wiki/Special:ListUsers/checkuser\">".($row2[2]?$row2[2]:0)."</td>\n";
+		echo "<td><a href=\"" . $row[1]. "/wiki/Special:ListUsers/oversight\">".($row2[3]?$row2[3]:0)."</td></tr>\n";
+	}
+	?>
+
+</tbody>
+</table>
+<br />
+<p>This tool was written by <a href="https://meta.wikimedia.org/wiki/User:Rschen7754">Rschen7754</a>. Acknowledgements to <a href="http://tools.wmflabs.org/erwin85/">erwin85</a> for the original tool and many of the queries, and to <a href="http://tools.wmflabs.org/pathoschild-contrib/">Pathoschild</a> for creating the extensive suite of tools that were used as an example.</p>
+</body>
+</html>
