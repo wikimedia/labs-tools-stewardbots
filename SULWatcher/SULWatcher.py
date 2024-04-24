wikimedia/labs-tools-stewardbots

commit e7a6f9e045e2fdfd9600e43f00a359ae7e9e64b6

SULWatcher: add PingServer mixin to handle ping timeouts
Using the default configuration of pings every 300s and disconnecting
after 2 fails. Each SULWatcher will maintain its own pings, as this is
the simplest and easiest solution. This was previously implemented for
StewardBot in 6a2ae8e and works well for keeping the bot connected to
IRC.

Change-Id: Icedcb49f132328c48e4d620f05d9d205e0878c50

SULWatcher/SULWatcher.py
@@ -20,7 +20,7 @@ import pymysql
 from ib3 import Bot
 from ib3.auth import SASL
 from ib3.connection import SSL
-from ib3.mixins import DisconnectOnError
+from ib3.mixins import DisconnectOnError, PingServer
 from ib3.nick import Ghost
 
 # Needs irc lib
@@ -84,7 +84,6 @@ class Querier:
 
 
 class SULWatcherException(Exception):
-
     """A single base exception class for all other SULWatcher errors."""
 
     def __init__(self, value):
@@ -95,27 +94,24 @@ class SULWatcherException(Exception):
 
 
 class CommanderError(SULWatcherException):
-
     """This exception is raised when the command parser fails."""
 
     pass
 
 
 class BotConnectionError(SULWatcherException):
-
     """This exception is raised when a bot has some connection error."""
 
     pass
 
 
 class ParseHostMaskError(SULWatcherException):
-
     """This exception is raised when a hostmask can't be parsed."""
 
     pass
 
 
-class LiberaBot(SASL, SSL, DisconnectOnError, Ghost, Bot):
+class LiberaBot(SASL, SSL, DisconnectOnError, Ghost, Bot, PingServer):
     def __init__(self, sulwatcher, channel, nickname, server, password, port=6697):
         self.sulwatcher = sulwatcher
         self.channel = channel
@@ -128,6 +124,8 @@ class LiberaBot(SASL, SSL, DisconnectOnError, Ghost, Bot):
             realname=nickname,
             ident_password=password,
             channels=[self.channel],
+            max_pings=2,
+            ping_interval=300,
         )
 
     def on_ctcp(self, c, event):

commit bf97679b64caf2d776f7d0dbd1a14eb9c6f5de2e

SULWatcher: Fix inheritance order for LiberaBot
Mixins have to come before Bot, otherwise everything dies in
a flaming ball of fire.

This commits changes already made on Toolforge to fix the error
introduced in e7a6f9e045e2fdfd9600e43f00a359ae7e9e64b6.

Change-Id: I3b66046f5fc9b6abab5ab591e331d143329bf7a4

SULWatcher/SULWatcher.py
@@ -111,7 +111,7 @@ class ParseHostMaskError(SULWatcherException):
     pass
 
 
-class LiberaBot(SASL, SSL, DisconnectOnError, Ghost, Bot, PingServer):
+class LiberaBot(SASL, SSL, DisconnectOnError, PingServer, Ghost, Bot):
     def __init__(self, sulwatcher, channel, nickname, server, password, port=6697):
         self.sulwatcher = sulwatcher
         self.channel = channel