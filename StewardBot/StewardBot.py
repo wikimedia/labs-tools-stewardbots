wikimedia/labs-tools-stewardbots

commit c8354da1ea34858d98bf0e08673697b15b94156c

StewardBot: add health check script based on canary events
This changeset adds a health check script (k8s liveliness probe) to
automatically restart the StewardBot pod if the RC reader fails.
https://wikitech.wikimedia.org/wiki/Help:Toolforge/Jobs_framework#Configuring_health-checks_for_jobs

If configured to do so, StewardBot will touch a /tmp file every time it
recieves a canary event. The health check script will then check if the
/tmp file exists and has not been modified for 65 minutes, restarting
the pod if so. If the /tmp file does not exist or has been touched
recently, nothing happens.

Focusing only on the hourly canary events will prevent unnecessary
restarts due to outages, but limits how quickly the bot will restart
after failures. If this proves to still be too slow, the conditions that
run heartbeat() can be changed in the future.

Change-Id: Ib2b36d0ce88a1a343d878f5d529de1505f1a7707

StewardBot/StewardBot.py
@@ -8,6 +8,7 @@ import threading
 import time
 from configparser import ConfigParser
 from datetime import datetime
+from pathlib import Path
 from typing import List, Tuple
 
 import config
@@ -1084,6 +1085,7 @@ class RecentChangesBot:
         self.stewards = query(queries["stewardusers"])
         self.should_exit = False
         self.RE_SECTION = re.compile(r"/\* *(?P<section>.+?) *\*/", re.DOTALL)
+        self.heartbeat_file = config.heartbeat_file
 
     def start(self):
         stream = "https://stream.wikimedia.org/v2/stream/recentchange"
@@ -1095,9 +1097,6 @@ class RecentChangesBot:
                         logger.info("Exiting EventStream listener")
                         break
 
-                    if bot1.quiet:
-                        continue
-
                     if event.event != "message":
                         continue
 
@@ -1107,6 +1106,12 @@ class RecentChangesBot:
                         continue
 
                     if change["meta"]["domain"] != "meta.wikimedia.org":
+                        if change["meta"]["domain"] == "canary":
+                            self.heartbeat()
+                        continue
+
+                    # Make sure to still process canary events while quiet
+                    if bot1.quiet:
                         continue
 
                     if change["bot"]:
@@ -1325,6 +1330,10 @@ class RecentChangesBot:
                 pass
             except Exception:
                 logger.exception("Recent changes listener encountered an error")
+
+    def heartbeat(self) -> None:
+        if self.heartbeat_file:
+            Path(self.hearbeat_file).touch()
 
     def get_changed_groups(
         self,

StewardBot/config_sample.py
@@ -10,3 +10,4 @@ server = "irc.libera.chat"
 dbname = "s51541_stewardbot"
 # Set to True to see random invitation message for annual Steward elections
 randmess = False
+heartbeat_file = ""

StewardBot/jobs.yaml
@@ -5,3 +5,4 @@
   emails: onfailure
   filelog-stdout: /data/project/stewardbots/logs/stewardbot.log
   filelog-stderr: /data/project/stewardbots/logs/stewardbot.log
+  health-check-script: /data/project/stewardbots/stewardbots/StewardBot/watchdog.sh

StewardBot/watchdog.sh
@@ -1,0 +1,10 @@
+#!/bin/bash
+
+MAX_MINUTES="65"
+HEARTBEAT_FILE="/tmp/stewardbot_heartbeat"
+
+if [[ $(find "$HEARTBEAT_FILE" -mmin "+$MAX_MINUTES" -print) ]]; then
+    # Heartbeat file exists and is older than MAX_MINUTES
+    ls -l --time-style=full-iso "$HEARTBEAT_FILE"
+    exit 1
+fi

commit ed1afde193a3055a05bb064a8f942bd4749a1ae8

StewardBot: fix typo in heartbeat()
Fix typo introduced in c8354da1ea34858d98bf0e08673697b15b94156c

Change-Id: Ifa624a2b8255e4a63cac219d3c4f17ff0622de92

StewardBot/StewardBot.py
@@ -1333,7 +1333,7 @@ class RecentChangesBot:
 
     def heartbeat(self) -> None:
         if self.heartbeat_file:
-            Path(self.hearbeat_file).touch()
+            Path(self.heartbeat_file).touch()
 
     def get_changed_groups(
         self,