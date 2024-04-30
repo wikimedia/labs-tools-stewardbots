wikimedia/labs-tools-stewardbots

commit 22f2a0500b92ed68eea6f065907c05911029be83

SULWatcher: attempt to catch EventStreams exceptions
This does not fix the 429 issue, but should prevent the need to
manually restart the RC reader most of the time.

Change-Id: I45ddcea8a1022d31fa97c4bc0dfc310d034ac713

SULWatcher/SULWatcher.py
@@ -784,6 +784,23 @@ class EventstreamsListener:
     def __init__(self, sulwatcher):
         self.sulwatcher = sulwatcher
         self.stop = threading.Event()
+        self.retries = 0
+
+    def event_stream(self, url):
+        """
+        Handle exceptions thrown by EventStream
+
+        We don't need to actually restart here, the while loop in start() will
+        take care of that.
+        """
+        try:
+            yield from EventStream(url)
+        except Exception as e:
+            logger.exception("RC reader error:", exc_info=e)
+            delay = 10 * 2**self.retires
+            logger.info(f"Sleeping for {delay} seconds")
+            self.retries += 1
+            time.sleep(delay)  # Wait a bit and hope EventStreams likes us
 
     def start(self):
         counter = 0
@@ -792,7 +809,8 @@ class EventstreamsListener:
         while (
             not self.stop.isSet()
         ):  # Thread will die when there isn't anything in the EventStream. Keep alive.
-            for event in EventStream(url):  # Listen to EventStream
+            for event in self.event_stream(url):  # Listen to EventStream
+                self.retries = 0
                 if self.stop.isSet():  # Check flag inside loop
                     break
                 if event.event != "message":

commit 01aace0451ae4a1ad8538c17a90a1244ddd55c58

SULWatcher: fix typo
retires -> retries
fix typo introduced in 22f2a0500b92ed68eea6f065907c05911029be83

Change-Id: I96dfc92c352f81609fdeeddd7e20b76e6f0875fe

SULWatcher/SULWatcher.py
@@ -795,7 +795,7 @@ class EventstreamsListener:
             yield from EventStream(url)
         except Exception as e:
             logger.exception("RC reader error:", exc_info=e)
-            delay = 10 * 2**self.retires
+            delay = 10 * 2**self.retries
             logger.info(f"Sleeping for {delay} seconds")
             self.retries += 1
             time.sleep(delay)  # Wait a bit and hope EventStreams likes us